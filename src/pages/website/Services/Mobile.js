de5a035e4d43943cf83c919114cde65c14c25ce6
StarMindz <64619873+StarMindz@users.noreply.github.com>
2023-03-27 07:33:31 +0100

    Complete landing page

--- src/pages/website/Services/Mobile.js
+++ src/pages/website/Services/Mobile.js
@@ -1,22 +1,29 @@
-import style from './Desktop.module.css';
+import style from './Mobile.module.css';
 import image1 from '../../../assets/desktop.png';
 
 const Mobile = () => {
   const html = (
     <div className={style.intro}>
-      <div className={style.main_images}>
-        <img className={style.image} src={image1} alt="Desktop app" />
-      </div>
       <div className={style.main_text}>
         <div className={style.main_text_cont}>
           <h1 className={style.main_heading}>
-            User-Friendly Health Dashboard
+            The ultimate health companion
+            <br />
+            Our patient app
           </h1>
           <p className={style.main_paragraph}>
-            Our dashboard makes it easy to track your patients progress,
-            stay informed, and make smart health decisions
+            Experience a personalized healthcare experience tailored to your unique needs,
+            all from the convenience of your mobile device.
+            <br />
+            <br />
+            You can securely share your medical information with authorized parties,
+            connect with healthcare professionals and fellow patients for support,
+            and even order medications and supplements directly from the app.
           </p>
         </div>
+      </div>
+      <div className={style.main_images}>
+        <img className={style.image} src={image1} alt="Desktop app" />
       </div>
     </div>
   );
